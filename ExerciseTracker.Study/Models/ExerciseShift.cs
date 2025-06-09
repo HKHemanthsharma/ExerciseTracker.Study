f675604c4f3018d25f805eaac45de4d34d059936
HKHemanthsharma <hkhemanthsharma@gmail.com>
2025-06-09 19:03:00 +0530

    Add project files.

---
+++ ExerciseTracker.Study/Models/ExerciseShift.cs
@@ -0,0 +1,12 @@
+﻿namespace ExerciseTracker.Study.Models
+{
+    public class ExerciseShift
+    {
+        public int ShiftId { get; set; }
+        public int ExerciseId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Comments { get; set; }
+    }
+}
