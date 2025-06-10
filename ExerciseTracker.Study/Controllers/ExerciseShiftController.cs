e25c29d90fe7542af4bb0f342cf2fdb7b0940983
HKHemanthsharma <hkhemanthsharma@gmail.com>
2025-06-10 23:56:27 +0530

    Implemented GET methods of ExerciseShifts Controller

---
+++ ExerciseTracker.Study/Controllers/ExerciseShiftController.cs
@@ -0,0 +1,31 @@
+﻿using ExerciseTracker.Study.Models;
+using ExerciseTracker.Study.Models.DTO;
+using ExerciseTracker.Study.Repositories;
+using ExerciseTracker.Study.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciseTracker.Study.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExerciseShiftController : ControllerBase
+    {
+        private readonly IService<ExerciseShift> ShiftService;
+        public ExerciseShiftController(IService<ExerciseShift> service)
+        {
+            ShiftService = service;
+        }
+        [HttpGet]
+        public async Task<ActionResult<ResponseDto<ExerciseShift>>> GetAllShifts()
+        {
+            return await ShiftService.GetAll();
+        }
+        [HttpGet]
+        [Route("{Id:int}")]
+        public async Task<ActionResult<ResponseDto<ExerciseShift>>> GetById([FromRoute]int Id)
+        {
+            return await ShiftService.GetById(Id);
+        }
+    }
+}
---
+++ ExerciseTracker.Study/Models/DTO/ExerciseShiftDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExerciseTracker.Study.Models.DTO
+{
+    public class ExerciseShiftDto
+    {
+        public int Id { get; set; }
+        public int ExerciseId { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string? Comments { get; set; }
+    }
+}
