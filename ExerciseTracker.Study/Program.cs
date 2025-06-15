6cf801ac8d963859bb19d8ff61b4cfcb8278163d
HKHemanthsharma <hkhemanthsharma@gmail.com>
2025-06-15 16:47:14 +0530

    Created Repository with Using Dapper for one of the Entities

--- ExerciseTracker.Study/Program.cs
+++ ExerciseTracker.Study/Program.cs
@@ -20,12 +20,13 @@ namespace ExerciseTracker.Study
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<ContextClass>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-            builder.Services.AddScoped<IRepository<Exercise>, RepositoryClass<Exercise>>();
+            builder.Services.AddScoped<IRepository<Exercise>, RepositoryDapper>();
            // builder.Services.AddScoped<IRepository<ExerciseDto>, RepositoryClass<ExerciseDto>>();
              builder.Services.AddScoped<IRepository<ExerciseShift>, RepositoryClass<ExerciseShift>>();
             builder.Services.AddScoped<IService<Exercise>, ServiceClass<Exercise>>();
             //builder.Services.AddScoped<IService<ExerciseDto>, ServiceClass<ExerciseDto>>();
             builder.Services.AddScoped<IService<ExerciseShift>, ServiceClass<ExerciseShift>>();
+            builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("ConnectionStrings"));
 
             var app = builder.Build();
 
